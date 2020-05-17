c0fa4607c3feb5a1ccfed05074438b7d23e3dfdd
Teo_Hwang <taeung2008@gmail.com>
2020-05-17 17:21:14 +0900

    💀 ver2 css complete

--- README.md
+++ README.md
@@ -1,24 +1,27 @@
 # CSS 연습용 사이트 만들기
- 이 레포는 반응형 웹사이트 및 CSS 와 javascript에 대한 연습을 하기 위해 만든 사이트이다.
 
- * 반응형(responsive) 웹 media query 연습
- * css positioning, display 연습
- * animation 및 transition
- * :root로 color 변수 생성
- * attribute 속성 이해
- * image 태그 float로 위치 조정 연습
- * javascript forEach와 this의 이해
- 
+이 레포는 반응형 웹사이트 및 CSS 와 javascript에 대한 연습을 하기 위해 만든 사이트이다.
+
+- 반응형(responsive) 웹 media query 연습
+- css positioning, display 연습
+- animation 및 transition
+- :root로 color 변수 생성
+- attribute 속성 이해
+- image 태그 float로 위치 조정 연습
+- javascript forEach와 this의 이해
 
 ## 사이트 설명
- 어벤져스 멤버 소개, 각 아이콘을 클릭하면 변경됨
- 
- <img src="https://github.com/Taewoong-H/ironman/blob/master/screen/1.png" width="300px" height="600px" title="px(픽셀) 크기 설정" alt="RubberDuck"></img><br/>
- <img src="https://github.com/Taewoong-H/ironman/blob/master/screen/3.PNG" width="600px" height="350px" title="px(픽셀) 크기 설정" alt="RubberDuck"></img><br/>
+
+어벤져스 멤버 소개, 각 아이콘을 클릭하면 변경됨
+
+<img src="https://github.com/Taewoong-H/ironman/blob/master/screen/1.png" width="300px" height="600px" title="ver1.1" alt="Avengers"></img><br/>
+<img src="https://github.com/Taewoong-H/ironman/blob/master/screen/3.PNG" width="600px" height="350px" title="ver1.1" alt="Avengers"></img><br/>
 
 ## 추가
- 연습용이므로 여러번 반복할 것이고 수정을 거칠 것이다.
- * 현재 1회차 완성. ver 1.1
- * 2회차 수정하며 반복 중.
- 
+
+연습용이므로 여러번 반복할 것이고 수정을 거칠 것이다.
+
+- 1회차 완성. ver 1.1
+- 2회차 css 완성. js 추가 예정
+
 https://taewoong-h.github.io/ironman/.
--- add.js
+++ add.js
@@ -1,91 +1,87 @@
 const skills = document.querySelectorAll('.skill'),
-    colors = document.querySelectorAll('.color'),
-    heros = document.querySelectorAll('.hero'),
-    gradients = document.querySelectorAll('.gradient');
-    heroBg = document.querySelector('.heroBackground');
+  colors = document.querySelectorAll('.color'),
+  heros = document.querySelectorAll('.hero'),
+  gradients = document.querySelectorAll('.gradient'),
+  heroBg = document.querySelector('.heroBackground');
 
-let prevColor = "blue";
+let prevColor = 'blue';
 let animationEnd = true;
 
 const number = document.querySelector('.number'),
-    num = number.querySelector('h1');
+  num = number.querySelector('h1');
 
-function changeSkill(){
-    skills.forEach(skill => skill.classList.remove('active'));
-    this.classList.add('active');
-    if(this.innerText === "1"){
-        num.innerText = "100";
-    } else if(this.innerText === "2"){
-        num.innerText = "200";
-    } else if(this.innerText === "3"){
-        num.innerText = "300";
-    } else if(this.innerText === "4"){
-        num.innerText = "400";
-    } else if(this.innerText === "5"){
-        num.innerText = "500";
-    }
+function changeSkill() {
+  skills.forEach((skill) => skill.classList.remove('active'));
+  this.classList.add('active');
+  if (this.innerText === '1') {
+    num.innerText = '100';
+  } else if (this.innerText === '2') {
+    num.innerText = '200';
+  } else if (this.innerText === '3') {
+    num.innerText = '300';
+  } else if (this.innerText === '4') {
+    num.innerText = '400';
+  } else if (this.innerText === '5') {
+    num.innerText = '500';
+  }
 }
 
-function changeColor(){
-    if(!animationEnd) return;
-    let primary = this.getAttribute('primary');
-    let color = this.getAttribute('color');
-    let hero = document.querySelector(`.hero[color="${color}"]`);
-    let gradient = document.querySelector(`.gradient[color="${color}"]`);
-    let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
-    let heroName = document.querySelector('.big');
-    
-    
-    colors.forEach(c => c.classList.remove('active'));
-    this.classList.add('active');
+function changeColor() {
+  if (!animationEnd) return;
+  let primary = this.getAttribute('primary');
+  let color = this.getAttribute('color');
+  let hero = document.querySelector(`.hero[color="${color}"]`);
+  let gradient = document.querySelector(`.gradient[color="${color}"]`);
+  let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
+  let heroName = document.querySelector('.big');
 
-    document.documentElement.style.setProperty('--primary', primary);
-    heros.forEach(h => h.classList.remove('show'));
-    hero.classList.add('show');
+  colors.forEach((c) => c.classList.remove('active'));
+  this.classList.add('active');
 
-    gradients.forEach(g => g.classList.remove('first', 'second'));
-    gradient.classList.add('first');
-    prevGradient.classList.add('second');
+  document.documentElement.style.setProperty('--primary', primary);
+  heros.forEach((h) => h.classList.remove('show'));
+  hero.classList.add('show');
 
-    prevColor = color;
+  gradients.forEach((g) => g.classList.remove('first', 'second'));
+  gradient.classList.add('first');
+  prevGradient.classList.add('second');
 
-    if(color === "blue"){
-        heroName.innerText = "Captain America";
-    } else if(color === "red"){
-        heroName.innerText = "Iron man";
-    } else if(color === "green"){
-        heroName.innerText = "Hulk";
-    } else if(color === "orange"){
-        heroName.innerText = "Spider man";
-    } else if(color === "black"){
-        heroName.innerText = "Thor";
-    }
+  prevColor = color;
 
+  if (color === 'blue') {
+    heroName.innerText = 'Captain America';
+  } else if (color === 'red') {
+    heroName.innerText = 'Iron man';
+  } else if (color === 'green') {
+    heroName.innerText = 'Hulk';
+  } else if (color === 'orange') {
+    heroName.innerText = 'Spider man';
+  } else if (color === 'black') {
+    heroName.innerText = 'Thor';
+  }
 
-    animationEnd = false;
+  animationEnd = false;
 
-    gradient.addEventListener('animationend', () => {
-        animationEnd = true;
-    })
+  gradient.addEventListener('animationend', () => {
+    animationEnd = true;
+  });
 }
 
+let x = window.matchMedia('(max-width: 1000px)');
 
-let x = window.matchMedia("(max-width: 1000px)");
-
-function changeHeight(){
-    if(x.matches){
-        let heroHeight = heros[0].offsetHeight;
-        heroBg.style.height = `${heroHeight * 0.9}px`;
-    } else{
-        heroBg.style.height = "475px";
-    }
+function changeHeight() {
+  if (x.matches) {
+    let heroHeight = heros[0].offsetHeight;
+    heroBg.style.height = `${heroHeight * 0.9}px`;
+  } else {
+    heroBg.style.height = '475px';
+  }
 }
 
-
-function init(){
-    skills.forEach(skill => skill.addEventListener('click', changeSkill));
-    colors.forEach(c => c.addEventListener('click', changeColor));
-    window.addEventListener('resize', changeHeight);
+function init() {
+  skills.forEach((skill) => skill.addEventListener('click', changeSkill));
+  colors.forEach((c) => c.addEventListener('click', changeColor));
+  window.addEventListener('resize', changeHeight);
 }
 
 init();
